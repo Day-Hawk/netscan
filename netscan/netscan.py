a5e486d334689cdf18bcf83f959c3214f04f6188
Day-Hawk <daniel.riethmueller@gmx.net>
2023-04-29 17:18:10 +0200

    dev: Implemented to if netscan.py will be started as main.

--- netscan/netscan.py
+++ netscan/netscan.py
@@ -34,17 +34,18 @@ def validate_connection(address: str, port: int = 80, timeout: int = 1):
 
         delta_time = (datetime.now() - request_time).total_seconds() * 1000
         __LOGGER.info(f'The connection({address}:{port}) can be established! [{delta_time:.3f}ms ping.]')
-        return __build_response(True, delta_time)  # Build response with running=True and no ping=delta_time
+        return response(True, delta_time)  # Build response with running=True and no ping=delta_time
     except socket.error:
         __LOGGER.error(f'The connection({address}:{port}) is not available!')
-        return __build_response(False)  # Build response with running=False and no ping=-1(invalid)
+        return response(False)  # Build response with running=False and no ping=-1(invalid)
         pass
 
     finally:
         __terminate_socket(test_socket)  # Close and kill socket with method
 
 
-def __build_response(running: bool, ping: float = -1):
+# Response dict of parameters.
+def response(running: bool, ping: float = -1):
     """
     Create dict with parameters.
 
@@ -56,6 +57,15 @@ def __build_response(running: bool, ping: float = -1):
         'running': running,
         'ping': ping
     }
+
+
+def port_in_range(port_to_check: int):
+    """
+
+    :param port_to_check:
+    :return:
+    """
+    return 0 < port_to_check < 65535
 
 
 def __terminate_socket(socket_to_kill: socket.socket):
@@ -76,49 +86,6 @@ def __terminate_socket(socket_to_kill: socket.socket):
     socket_to_kill.close()  # At least close socket.
     __LOGGER.debug('Successfully killed socket.')
 
-
-# Main start method
-def __start():
-    """
-    Start method with parameters.
-    :return: None
-    """
-    __LOGGER.info(f'Starting NetScan v{info.VERSION} ...')
-
-    import sys
-    __LOGGER.info(f'OS platform: {sys.platform}. (Py-Version: {sys.version.split(" ")[0]})')
-
-    host: str = 'localhost'
-    port: int = 8529
-
-    validate_connection(address=host, port=port)
-
-
-"""
-    __LOGGER.info('Waiting for input with valid host.')
-    while host == '':
-        try:
-            __input = str(input())
-
-            if __input == '':
-                __input = 'localhost'
-
-            host = __input
-        except:
-            continue
-
-    __LOGGER.info(f'Waiting for input with valid port for host {host}.')
-
-    while port < 0:
-        try:
-            __input = str(input())
-
-            __input_int = 80 if __input == '' else int(input())
-
-            port = __input_int
-        except:
-            continue
-"""
 
 __ENV_HOST: Final[str] = 'NETSCAN_HOST'
 __ENV_PORT: Final[str] = 'NETSCAN_PORT'
@@ -142,16 +109,50 @@ def __collect_environment():
         raise RuntimeError('Port is not a number.')
 
 
-def __build_object(host: str, port: int):
-    """
-
-    :param host:
-    :param port:
-    :return:
-    """
-    return {'host': host, 'port': port}
-
-
-# Start statement
+# Start statement if this file was called as main
 if __name__ == '__main__':
-    __start()  # Call start
+    import sys
+
+    __LOGGER.info('Since "netscan.py" has been executed, it can be operated directly with the input')
+    __LOGGER.info(f'Starting NetScan v{info.VERSION} ...')
+    __LOGGER.info(f'OS platform: {sys.platform}. (Py-Version: {sys.version.split(" ")[0]})')
+
+    __LOGGER.info('<CTRL + C> or type "kill" to terminate program.')
+    __LOGGER.info('Type "clear" to remove configuration.')
+
+    host: str = ''
+    port: int = -1
+
+    __LOGGER.info(f'Enter host:')
+    while True:
+        console_input = input().lower()
+        if console_input == 'kill':
+            exit()
+        elif console_input == 'clear':
+            host = ''
+            port = -1
+            __LOGGER.info(f'Cleared configuration, to start over enter host:')
+            continue
+
+        if host == '':
+            host = console_input
+            __LOGGER.info(f'Set {host} as host, enter port to scan:')
+            continue
+
+        if port is not port_in_range(port):
+            try:
+                port_input = int(console_input)
+
+                if port_in_range(port_input):
+                    port = port_input
+                    __LOGGER.info(f'Set {port} as port.')
+
+                    validate_connection(address=host, port=port)
+                    host = ''
+                    port = -1
+                    __LOGGER.info(f'Waiting for next host:')
+                else:
+                    __LOGGER.error(f'Given port {port_input} is not in range 1...65535.')
+            except ValueError:
+                __LOGGER.error(f'{console_input} is not a number.')
+            continue
