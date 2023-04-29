19e5bc38ec5951b602e16b08990501b534c0e9a9
Day-Hawk <daniel.riethmueller@gmx.net>
2023-04-29 12:51:28 +0200

    dev: Changed __VERSION to str, added some docs and a dict to combine response result and ping.

--- netscan.py
+++ netscan.py
@@ -13,7 +13,7 @@ Default logger for system.
 __LOGGER: Final[log] = log(module_name='NetScan')  # Create logger
 
 #  Version of server
-__VERSION: Final[float] = 1.0
+__VERSION: Final[str] = '1.0.0'
 
 
 def validate_connection(address: str, port: int = 80, timeout: int = 1):
@@ -23,9 +23,9 @@ def validate_connection(address: str, port: int = 80, timeout: int = 1):
     :param address: addressed by the request.
     :param port: of the application from the address.
     :param timeout: Time to wait for response of partner.
-    :return: True, if connection is valid. False if connection is not available or valid.
+    :return:
     """
-    test_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    test_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Instance of socket.
     request_time = datetime.now()
     try:
         __LOGGER.debug(f'Try to connect to {address}, {port}. [Timeout: {timeout}s]')
@@ -33,17 +33,30 @@ def validate_connection(address: str, port: int = 80, timeout: int = 1):
         test_socket.settimeout(timeout)  # Define timeout parameter.
         test_socket.connect((address, port))  # Use socket to connect to given address and port.
 
-        response_time = datetime.now()
-        delta_time = (response_time - request_time).total_seconds() * 1000
+        delta_time = (datetime.now() - request_time).total_seconds() * 1000
         __LOGGER.info(f'The connection({address}:{port}) can be established! [{delta_time:.3f}ms ping.]')
-        return True
+        return __build_response(True, delta_time)  # Build response with running=True and no ping=delta_time
     except socket.error:
         __LOGGER.error(f'The connection({address}:{port}) is not available!')
-        return False
+        return __build_response(False)  # Build response with running=False and no ping=-1(invalid)
         pass
 
     finally:
         __terminate_socket(test_socket)  # Close and kill socket with method
+
+
+def __build_response(running: bool, ping: float = -1):
+    """
+    Create dict with parameters.
+
+    :param running: True, if connection is valid. False if connection is not available or valid.
+    :param ping: Given time for request and response in ms.
+    :return: Object combining running and ping.
+    """
+    return {
+        'running': running,
+        'ping': ping
+    }
 
 
 def __terminate_socket(socket_to_kill: socket.socket):
@@ -76,9 +89,13 @@ def __start():
     import sys
     __LOGGER.info(f'OS platform: {sys.platform}. (Py-Version: {sys.version.split(" ")[0]})')
 
-    host: str = ''
-    port: int = -1
+    host: str = 'localhost'
+    port: int = 8529
 
+    validate_connection(address=host, port=port)
+
+
+"""
     __LOGGER.info('Waiting for input with valid host.')
     while host == '':
         try:
@@ -102,9 +119,7 @@ def __start():
             port = __input_int
         except:
             continue
-
-    validate_connection(address=host, port=port)
-
+"""
 
 __ENV_HOST: Final[str] = 'NETSCAN_HOST'
 __ENV_PORT: Final[str] = 'NETSCAN_PORT'
