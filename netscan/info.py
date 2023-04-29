905deba662f0d0414ef56ff2b3f20d05ec15fc2c
Day-Hawk <daniel.riethmueller@gmx.net>
2023-04-29 17:17:45 +0200

    dev: Added docs to info.py.

--- netscan/info.py
+++ netscan/info.py
@@ -1,14 +1,18 @@
 from typing import Final
 
+# Name of program.
 NAME: Final[str] = 'netscan'
 
-#  Version
+# Version of code.
 VERSION: Final[str] = '1.0.0'
 
+# Description of program.
 LONG_DESCRIPTION: Final[str] = ''
 SHORT_DESCRIPTION: Final[str] = ''
 
+# Needed dependencies for program.
 DEPENDENCIES: Final = ['fire', 'log4python']
 TEST_DEPENDENCIES: Final = []
 
+# Origin of code.
 URL: Final[str] = 'https://github.com/Day-Hawk/netscan'
