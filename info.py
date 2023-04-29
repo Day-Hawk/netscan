fc4dd8706489030bc052566704ae6d575a4d6297
Day-Hawk <daniel.riethmueller@gmx.net>
2023-04-29 13:12:18 +0200

    dev: Added info.py to store project info.

---
+++ info.py
@@ -0,0 +1,15 @@
+from typing import Final
+
+NAME: Final[str] = 'netscan'
+
+#  Version
+VERSION: Final[str] = '1.0.0'
+
+
+LONG_DESCRIPTION: Final[str] = ''
+SHORT_DESCRIPTION: Final[str] = ''
+
+DEPENDENCIES: Final = ['fire', 'log4python']
+TEST_DEPENDENCIES: Final = []
+
+URL: Final[str] = 'https://github.com/Day-Hawk/netscan'
